adc81e8261f0b26509c055942a561ee73967054d
Giovanni Morelli <116790906+giovimori@users.noreply.github.com>
2023-05-08 16:33:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using SQLite;
+
+namespace LinqDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var cn1 = new SQLiteConnection("chinook.db"))
+            {
+                cn1.CreateTable<Artist>();
+
+                var artists = cn1.Table<Artist>().OrderBy(a => a.ArtistId).ThenBy(a => a.Name);
+
+                Console.WriteLine("Artists:");
+               
+            
+
+                foreach (var artist in artists)
+                {
+                    Console.WriteLine($"ArtistId: {artist.ArtistId}\tName: {artist.Name}");
+                }
+            }
+        }
+    }
+
+    public class Artist
+    {
+        [PrimaryKey]
+        public int ArtistId { get; set; }
+        public string Name { get; set; }
+    }
+}
